4927032ec4b94371d63b4e3a1584f3d0b46a7ec5
Lwt235 <m15955295202@163.com>
2025-12-11 21:50:29 +0800

    适配推理代码

---
+++ inference.py
@@ -0,0 +1,16 @@
+from inference import VideoTemporalInference
+
+# Initialize
+engine = VideoTemporalInference(
+    model_path="./ckpts/Qwen3-VL-4B-Instruct",
+)
+
+# Predict
+result = engine.predict(
+    video_path="./data/videos/timerft_data/_0yiT0hhCCM_00:06:44:200_00:07:09:100.mp4",
+    query="the paper bill has an image of a woman holding up a paper",
+    video_start=404.2,
+    video_end=429.1
+)
+
+print(f"Segment: {result['start_time']:.2f}s - {result['end_time']:.2f}s")
